701769ae84cc8f19ad2b25a085c964fe616657b0
eltonpeniche <elton_nfs@hotmail.com>
2018-12-11 14:06:29 -0300

    change matrix type to long double

--- add_matrix_mpi.c
+++ add_matrix_mpi.c
@@ -4,7 +4,7 @@
 
 #define LIMITE 500
 
-int init_matrix(int m[][LIMITE], int valor){
+long double init_matrix(long double m[][LIMITE], int valor){
 	int i, j;
 	for (i = 0; i < LIMITE; i = i + 1){
 		for (j = 0; j < LIMITE; j = j + 1){
@@ -14,17 +14,17 @@ int init_matrix(int m[][LIMITE], int valor){
  	}
 }
 
-int mostrar_matriz(int m[][LIMITE]){
+long double mostrar_matriz(long double m[][LIMITE]){
 	int i, j;
 	for (i = 0; i < LIMITE; i = i + 1){
 		printf("\n");
 		for (j = 0; j < LIMITE; j = j + 1) {
-			printf("%d ", m[i][j]);	
+			printf("%LG ", m[i][j]);	
 		}
 	}
 }
 
-int somar_matriz(int A[][LIMITE], int B[][LIMITE], int C[][LIMITE], int ii, int il, int ji, int jl){
+long double somar_matriz(long double A[][LIMITE], long double B[][LIMITE], long double C[][LIMITE], int ii, int il, int ji, int jl){
 	int i, j;
 	for (i = ii; i < il; i++){
 		for (j = ji; j < jl; j++){
@@ -35,7 +35,7 @@ int somar_matriz(int A[][LIMITE], int B[][LIMITE], int C[][LIMITE], int ii, int 
 }
 
 
-int merge_matrix(int A[][LIMITE], int B[][LIMITE], int ii, int il, int ji, int jl){
+long double merge_matrix(long double A[][LIMITE], long double B[][LIMITE], int ii, int il, int ji, int jl){
 	int i, j;
 	for (i = ii; i < il; i++){
 		for (j = ji; j < jl; j++){
@@ -61,13 +61,13 @@ int main(int argc, char** argv) {
 		MPI_Abort(MPI_COMM_WORLD, 1);
 	}
 	
-	int A[LIMITE][LIMITE];
+	long double A[LIMITE][LIMITE];
 	init_matrix(A,1);
 	
-	int B[LIMITE][LIMITE];
+	long double B[LIMITE][LIMITE];
 	init_matrix(B,2);
 	
-	int C[LIMITE][LIMITE];
+	long double C[LIMITE][LIMITE];
 	init_matrix(C,0);
 
 	int number;
@@ -77,15 +77,15 @@ int main(int argc, char** argv) {
 		somar_matriz(A,B,C,0,LIMITE/2,0,LIMITE/2);
 		//mostrar_matriz(C);
 
-		int C2[LIMITE][LIMITE];
+		long double C2[LIMITE][LIMITE];
 
-		MPI_Recv(C2, LIMITE*LIMITE, MPI_INT, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		MPI_Recv(C2, LIMITE*LIMITE, MPI_LONG_DOUBLE, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 		merge_matrix(C, C2, 0,LIMITE/2,LIMITE/2,LIMITE);
 
-		MPI_Recv(C2, LIMITE*LIMITE, MPI_INT, 2, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		MPI_Recv(C2, LIMITE*LIMITE, MPI_LONG_DOUBLE, 2, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 		merge_matrix(C, C2, LIMITE/2,LIMITE,0,LIMITE/2);
 
-		MPI_Recv(C2, LIMITE*LIMITE, MPI_INT, 3, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		MPI_Recv(C2, LIMITE*LIMITE, MPI_LONG_DOUBLE, 3, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 		merge_matrix(C, C2, LIMITE/2,LIMITE,LIMITE/2,LIMITE);
 
 		mostrar_matriz(C);
@@ -94,19 +94,19 @@ int main(int argc, char** argv) {
 		
 		somar_matriz(A,B,C,0,LIMITE/2,LIMITE/2,LIMITE);
 		
-		MPI_Send(C, LIMITE*LIMITE, MPI_INT, 0, 0, MPI_COMM_WORLD);
+		MPI_Send(C, LIMITE*LIMITE, MPI_LONG_DOUBLE, 0, 0, MPI_COMM_WORLD);
 		
 	}else if (world_rank == 2) {
 		
 		somar_matriz(A,B,C,LIMITE/2,LIMITE,0,LIMITE/2);
 		
-		MPI_Send(C, LIMITE*LIMITE, MPI_INT, 0, 0, MPI_COMM_WORLD);
+		MPI_Send(C, LIMITE*LIMITE, MPI_LONG_DOUBLE, 0, 0, MPI_COMM_WORLD);
 		
 	}else if (world_rank == 3) {
 		
 		somar_matriz(A,B,C,LIMITE/2,LIMITE,LIMITE/2,LIMITE);
 		
-		MPI_Send(C, LIMITE*LIMITE, MPI_INT, 0, 0, MPI_COMM_WORLD);
+		MPI_Send(C, LIMITE*LIMITE, MPI_LONG_DOUBLE, 0, 0, MPI_COMM_WORLD);
 		
 	}
 MPI_Finalize();
